6c401e81fb06b002e30a96f8a0da383bdba87fb0
Adrian Vazquez <aavm0000@gmail.com>
2020-10-09 17:21:26 -0400

    update: task created

--- main/main.c
+++ main/main.c
@@ -3,15 +3,30 @@
  * Date: 10-07-2020
  */
 
-#include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#define driver "driver/gpio.h"
+#include "esp_log.h"
 #include <stdio.h>
-char *TAG = "INFO2";
-void app_main() {
 
-  while (1) {
-    ESP_LOGI(TAG, "Hello World");
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-  }
+#define PULSE_PERIPHERAL
+char *TAG = "INFO";
+
+void task_pulse(void *arg) {
+    ESP_LOGI(TAG, "Pulse counter taks Started");
+
+    while (1) {
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+void app_main() {
+    ESP_LOGI(TAG, "MCU initialized");
+
+#ifdef PULSE_PERIPHERAL
+    ESP_LOGI(TAG, "Start peripheral");
+    xTaskCreatePinnedToCore(task_pulse, "task_pulse", 1024 * 2, NULL, 10, NULL,
+                            0);
+#endif
 }
