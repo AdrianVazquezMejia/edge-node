6b2b66ca93e1f82251e5cbff9e6797e30ae95c7a
Adrian Vazquez <aavm0000@gmail.com>
2020-10-09 17:57:06 -0400

    feat: pin counter counts

--- main/main.c
+++ main/main.c
@@ -15,9 +15,22 @@ char *TAG = "INFO";
 void task_pulse(void *arg) {
     ESP_LOGI(TAG, "Pulse counter task started");
     gpio_set_direction(GPIO_NUM_0, GPIO_MODE_INPUT);
+    int pinLevel;
+    int pulses   = 0;
+    bool counted = false;
+
     while (1) {
-        ESP_LOGI(TAG, "level is: %d", gpio_get_level(GPIO_NUM_0));
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        pinLevel = gpio_get_level(GPIO_NUM_0);
+        if (pinLevel == 1 && counted == false) {
+            pulses++;
+            counted = true;
+            ESP_LOGI(TAG, "PULSE!... level is: %d", pinLevel);
+        }
+        if (pinLevel == 0 && counted == true) {
+            counted = false;
+            ESP_LOGI(TAG, "level is: %d", pinLevel);
+        }
+        vTaskDelay(10 / portTICK_PERIOD_MS);
     }
 }
 
