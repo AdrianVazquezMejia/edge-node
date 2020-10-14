449a3cb1e597bb2c858855f4a6ce574a7550b209
Adrian Vazquez <aavm0000@gmail.com>
2020-10-14 13:17:25 -0400

    feat: modbus slave read input register

--- main/include/modbus_slave.h
+++ main/include/modbus_slave.h
@@ -9,6 +9,17 @@
 #define MAIN_INCLUDE_MODBUS_SLAVE_H_
 #include "freertos/FreeRTOS.h"
 #include "freertos/semphr.h"
+typedef union {
+    uint16_t Val;
+    struct {
+        uint8_t LB;
+        uint8_t HB;
+    } byte;
+
+} INT_VAL;
+enum modbus_function_t { READ_HOLDING = 3, READ_INPUT };
 void uart_init(QueueHandle_t *queue);
+void modbus_slave_functions(const uint8_t *frame, uint8_t lengh,
+                            uint16_t **modbus_registers);
 
 #endif /* MAIN_INCLUDE_MODBUS_SLAVE_H_ */
