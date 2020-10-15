530fe9b441227e87f213885f38069bbbf21dd8a1
Adrian Vazquez <aavm0000@gmail.com>
2020-10-15 16:32:11 -0400

    feat: watch dog task added
    9s timer watchdog
    10s task watchdog

--- main/main.c
+++ main/main.c
@@ -7,6 +7,7 @@
 #include "driver/gpio.h"
 #include "driver/uart.h"
 #include "esp_log.h"
+#include "esp_task_wdt.h"
 #include "flash.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -14,7 +15,6 @@
 #include "modbus_slave.h"
 #include "strings.h"
 #include <stdio.h>
-
 #define PULSES_KW 225
 
 #define RX_BUF_SIZE 1024
@@ -24,9 +24,21 @@
 
 #define TIME_SCAN  2000
 #define MAX_SLAVES 255
+
+#define TWDT_TIMEOUT_S 10
+
+#define CHECK_ERROR_CODE(returned, expected)                                   \
+    ({                                                                         \
+        if (returned != expected) {                                            \
+            printf("TWDT ERROR\n");                                            \
+            abort();                                                           \
+        }                                                                      \
+    })
 static char *TAG = "INFO";
 
 void task_pulse(void *arg) {
+    CHECK_ERROR_CODE(esp_task_wdt_add(NULL), ESP_OK);
+    CHECK_ERROR_CODE(esp_task_wdt_status(NULL), ESP_OK);
     ESP_LOGI(TAG, "Pulse counter task started");
     gpio_set_direction(GPIO_NUM_0, GPIO_MODE_INPUT);
     int pinLevel;
@@ -45,9 +57,12 @@ void task_pulse(void *arg) {
             counted = false;
         }
         vTaskDelay(10 / portTICK_PERIOD_MS);
+        CHECK_ERROR_CODE(esp_task_wdt_reset(), ESP_OK);
     }
 }
 void task_modbus_slave(void *arg) {
+    CHECK_ERROR_CODE(esp_task_wdt_add(NULL), ESP_OK);
+    CHECK_ERROR_CODE(esp_task_wdt_status(NULL), ESP_OK);
     QueueHandle_t uart_queue;
     uart_event_t event;
     uint8_t *dtmp = (uint8_t *)malloc(RX_BUF_SIZE);
@@ -83,12 +98,15 @@ void task_modbus_slave(void *arg) {
         default:
             ESP_LOGI(TAG, "UART event");
         }
+        CHECK_ERROR_CODE(esp_task_wdt_reset(), ESP_OK);
     }
     free(dtmp);
     dtmp = NULL;
 }
 
 static void task_modbus_master(void *arg) {
+    CHECK_ERROR_CODE(esp_task_wdt_add(NULL), ESP_OK);
+    CHECK_ERROR_CODE(esp_task_wdt_status(NULL), ESP_OK);
     QueueHandle_t uart_queue;
     uart_event_t event;
     uint8_t *dtmp = (uint8_t *)malloc(RX_BUF_SIZE);
@@ -106,13 +124,13 @@ static void task_modbus_master(void *arg) {
         if (slaves[curr_slave]) {
             read_input_register(curr_slave, start_add, quantity);
 
-            if (xQueuePeek(uart_queue, (void *)&event, (portTickType)1000)) {
+            if (xQueuePeek(uart_queue, (void *)&event, (portTickType)100)) {
                 ESP_LOGI(TAG, "Cleaned");
                 if (event.type == UART_BREAK) {
                     xQueueReset(uart_queue);
                 }
             }
-            if (xQueueReceive(uart_queue, (void *)&event, (portTickType)1000)) {
+            if (xQueueReceive(uart_queue, (void *)&event, (portTickType)100)) {
                 switch (event.type) {
                 case UART_DATA:
                     uart_read_bytes(UART_NUM_1, dtmp, event.size,
@@ -136,14 +154,15 @@ static void task_modbus_master(void *arg) {
             } else
                 ESP_LOGI(TAG, "Timeout");
             vTaskDelay(TIME_SCAN / portTICK_PERIOD_MS);
-        } else {
-            vTaskDelay(10 / portTICK_PERIOD_MS);
         }
         curr_slave++;
+        CHECK_ERROR_CODE(esp_task_wdt_reset(), ESP_OK);
     }
 }
 void app_main() {
     ESP_LOGI(TAG, "MCU initialized");
+    ESP_LOGI(TAG, "Init Watchdog");
+    CHECK_ERROR_CODE(esp_task_wdt_init(TWDT_TIMEOUT_S, true), ESP_OK);
 #ifdef CONFIG_PULSE_PERIPHERAL
     ESP_LOGI(TAG, "Start peripheral");
     xTaskCreatePinnedToCore(task_pulse, "task_pulse", 1024 * 2, NULL, 10, NULL,
